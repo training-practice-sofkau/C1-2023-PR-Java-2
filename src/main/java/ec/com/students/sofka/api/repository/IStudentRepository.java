1367bbbc343926f2814baa55c0acee5949430dd9
Diego Becerra <diegofer_1110@hotmail.com>
2023-03-14 15:54:47 -0500

    repository done

---
+++ src/main/java/ec/com/students/sofka/api/repository/IStudentRepository.java
@@ -0,0 +1,9 @@
+package ec.com.students.sofka.api.repository;
+
+import ec.com.students.sofka.api.domain.collection.Student;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IStudentRepository extends ReactiveMongoRepository<Student,String> {
+}
