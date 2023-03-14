229caf38d36987e825cd1276fcd9560b226ffe3f
Larry Mateo Ramirez C <larry.ramirez@correounivalle.edu.co>
2023-03-14 14:38:53 -0500

    Crate the resource layer

---
+++ src/main/java/ec/com/students/sofka/api/resource/StudentResource.java
@@ -0,0 +1,28 @@
+package ec.com.students.sofka.api.resource;
+
+import ec.com.students.sofka.api.domain.dto.StudentDTO;
+import ec.com.students.sofka.api.service.impl.StudentServiceImpl;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RestController
+@AllArgsConstructor
+public class StudentResource {
+
+    private StudentServiceImpl studentService;
+
+    @GetMapping("/students")
+    private Mono<ResponseEntity<Flux<StudentDTO>>> getAllStudents() {
+        return studentService
+                .getAllStudents()
+                .switchIfEmpty(Flux.error(new Throwable(HttpStatus.NO_CONTENT.toString())))
+                .collectList()
+                .map(studentDTOS -> new ResponseEntity<>(Flux.fromIterable(studentDTOS), HttpStatus.OK))
+                .onErrorResume(throwable -> Mono.just(new ResponseEntity<>(HttpStatus.NO_CONTENT)));
+    }
+}
