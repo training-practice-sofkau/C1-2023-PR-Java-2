aaff100e1d43563ae4ae36179638d59b6b23bd50
Mishell Yagual <54877658+mdyagual@users.noreply.github.com>
2023-03-14 11:03:35 -0500

    first commit

---
+++ src/main/java/ec/com/students/sofka/api/StudentsApiApplication.java
@@ -0,0 +1,13 @@
+package ec.com.students.sofka.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ec/com/students/sofka/api/StudentsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.com.students.sofka.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
