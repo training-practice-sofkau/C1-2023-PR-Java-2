02795872a68e8b4eb63cd69c842c0276ed4b7889
Juangmcz <juangui-089@hotmail.com>
2023-03-14 14:02:51 -0500

    Student class was added.

---
+++ src/main/java/ec/com/students/sofka/api/domain/collection/Student.java
@@ -0,0 +1,29 @@
+package ec.com.students.sofka.api.domain.collection;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import javax.validation.constraints.NotNull;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Data
+//@AllArgsConstructor
+@NoArgsConstructor
+@Document(collection = "students")
+public class Student {
+
+    @Id
+    private String id = UUID.randomUUID().toString().substring(0, 10);
+    private String idNum;
+    @NotNull(message = "The name cannot be null")
+    private String name;
+    @NotNull(message = "The last name cannot be null")
+    private String lastName;
+    private boolean blocked =  false;
+    private List<String> books = new ArrayList<>();
+
+}
