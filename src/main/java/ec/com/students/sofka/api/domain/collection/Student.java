02d1253a001964644d2855edeecfafb225199352
BrianSammit <briansammit.cruz@gmail.com>
2023-03-15 00:28:43 -0500

    Add student dto

--- src/main/java/ec/com/students/sofka/api/domain/collection/Student.java
+++ src/main/java/ec/com/students/sofka/api/domain/collection/Student.java
@@ -6,7 +6,6 @@ import org.springframework.data.mongodb.core.mapping.Document;
 
 import javax.persistence.Id;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
 import java.util.UUID;
 
 @Data
