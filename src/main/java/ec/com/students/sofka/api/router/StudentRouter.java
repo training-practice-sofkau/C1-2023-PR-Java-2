613c883fe7ed970ac3ca7d3de50f20e982b4aa52
Julian Rojas <julianrojasgeo@gmail.com>
2023-03-16 18:49:38 -0500

    Student not found fixed for no student found case in the router

--- src/main/java/ec/com/students/sofka/api/router/StudentRouter.java
+++ src/main/java/ec/com/students/sofka/api/router/StudentRouter.java
@@ -1,10 +1,7 @@
 package ec.com.students.sofka.api.router;
 
 import ec.com.students.sofka.api.domain.dto.StudentDTO;
-import ec.com.students.sofka.api.usecases.DeleteStudentUseCase;
-import ec.com.students.sofka.api.usecases.GetAllStudentsUseCase;
-import ec.com.students.sofka.api.usecases.GetStudentByIdUseCase;
-import ec.com.students.sofka.api.usecases.SaveStudentUseCase;
+import ec.com.students.sofka.api.usecases.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
@@ -12,6 +9,8 @@ import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import static org.springframework.web.reactive.function.server.RequestPredicates.*;
 import static org.springframework.web.reactive.function.server.RouterFunctions.route;
@@ -32,6 +31,7 @@ public class StudentRouter {
     public RouterFunction<ServerResponse> getStudentById(GetStudentByIdUseCase getStudentByIdUseCase){
         return route(GET("/students/{id}"),
                 request -> getStudentByIdUseCase.apply(request.pathVariable("id"))
+                        .switchIfEmpty(Mono.error(new Throwable(HttpStatus.NO_CONTENT.toString())))
                         .flatMap(studentDTO -> ServerResponse.ok()
                                 .contentType(MediaType.APPLICATION_JSON)
                                 .bodyValue(studentDTO))
@@ -43,6 +43,17 @@ public class StudentRouter {
         return route(POST("/students").and(accept(MediaType.APPLICATION_JSON)),
                 request -> request.bodyToMono(StudentDTO.class)
                         .flatMap(studentDTO -> saveStudentUseCase.apply(studentDTO)
+                                .flatMap(result -> ServerResponse.status(201)
+                                        .contentType(MediaType.APPLICATION_JSON)
+                                        .bodyValue(result))
+                                .onErrorResume(throwable -> ServerResponse.status(HttpStatus.NOT_ACCEPTABLE).build())));
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> updateStudent(UpdateStudentUseCase updateStudentUseCase){
+        return route(PUT("/students/{id}").and(accept(MediaType.APPLICATION_JSON)),
+                request -> request.bodyToMono(StudentDTO.class)
+                        .flatMap(studentDTO -> updateStudentUseCase.apply(request.pathVariable("id"), studentDTO)
                                 .flatMap(result -> ServerResponse.status(201)
                                         .contentType(MediaType.APPLICATION_JSON)
                                         .bodyValue(result))
--- src/main/java/ec/com/students/sofka/api/usecases/UpdateStudentUseCase.java
+++ src/main/java/ec/com/students/sofka/api/usecases/UpdateStudentUseCase.java
@@ -1,5 +1,6 @@
 package ec.com.students.sofka.api.usecases;
 
+import ec.com.students.sofka.api.domain.collection.Student;
 import ec.com.students.sofka.api.domain.dto.StudentDTO;
 import ec.com.students.sofka.api.repository.StudentRepository;
 import lombok.AllArgsConstructor;
@@ -17,7 +18,16 @@ public class UpdateStudentUseCase implements BiFunction<String, StudentDTO, Mono
     private final ModelMapper mapper;
 
     @Override
-    public Mono<StudentDTO> apply(String s, StudentDTO studentDTO) {
-        return null;
+    public Mono<StudentDTO> apply(String ID, StudentDTO studentDTO) {
+        return this.studentRepository
+                .findById(ID)
+                .switchIfEmpty(Mono.empty())
+                .map(student ->{
+                    studentDTO.setId(student.getId());
+                    return mapper.map(
+                            studentRepository
+                                    .save(mapper.map(studentDTO, Student.class)),
+                            StudentDTO.class);
+                });
     }
 }
