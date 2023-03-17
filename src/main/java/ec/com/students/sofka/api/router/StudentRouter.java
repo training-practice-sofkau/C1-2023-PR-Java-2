c4c1ef39020baa8cce0e11bf3992a45fc2de9b70
Juangmcz <juangui-089@hotmail.com>
2023-03-17 08:07:53 -0500

    StudentRouter was implemented.

--- src/main/java/ec/com/students/sofka/api/router/StudentRouter.java
+++ src/main/java/ec/com/students/sofka/api/router/StudentRouter.java
@@ -1,4 +1,70 @@
 package ec.com.students.sofka.api.router;
 
+import ec.com.students.sofka.api.domain.dto.StudentDTO;
+import ec.com.students.sofka.api.usecases.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.*;
+import static org.springframework.web.reactive.function.server.RequestPredicates.DELETE;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+
+@Configuration
 public class StudentRouter {
+
+    @Bean
+    public RouterFunction<ServerResponse> getAllStudents(GetAllStudentsUseCase getAllStudentsUseCase){
+        return route(GET("api/students"),
+                request -> ServerResponse.ok()
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .body(BodyInserters.fromPublisher(getAllStudentsUseCase.get(), StudentDTO.class))
+                        .onErrorResume(throwable -> ServerResponse.noContent().build()));
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> getStudentById(GetStudentByIdUseCase getStudentByIdUseCase){
+        return route(GET("api/students/{id}"),
+                request -> getStudentByIdUseCase.apply(request.pathVariable("id"))
+                        .flatMap(studentDTO -> ServerResponse.ok()
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .bodyValue(studentDTO))
+                        .onErrorResume(throwable -> ServerResponse.notFound().build()));
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> saveStudent(SaveStudentUseCase saveStudentUseCase){
+        return route(POST("api/students").and(accept(MediaType.APPLICATION_JSON)),
+                request -> request.bodyToMono(StudentDTO.class)
+                        .flatMap(studentDTO -> saveStudentUseCase.apply(studentDTO)
+                                .flatMap(result -> ServerResponse.status(201)
+                                        .contentType(MediaType.APPLICATION_JSON)
+                                        .bodyValue(result))
+                                .onErrorResume(throwable -> ServerResponse.status(HttpStatus.NOT_ACCEPTABLE).build())));
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> updateStudent(UpdateStudentUseCase updateStudentUseCase){
+        return route(PUT("api/students/{id}").and(accept(MediaType.APPLICATION_JSON)),
+                request -> request.bodyToMono(StudentDTO.class)
+                        .flatMap(studentDTO -> updateStudentUseCase.apply(request.pathVariable("id"), studentDTO)
+                                .flatMap(result -> ServerResponse.status(200)
+                                        .contentType(MediaType.APPLICATION_JSON)
+                                        .bodyValue(result))
+                                .onErrorResume(throwable -> ServerResponse.status(HttpStatus.NOT_FOUND).build())));
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> deleteStudent(DeleteStudentUseCase deleteStudentUseCase){
+        return route(DELETE("api/students/{id}"),
+                request ->  deleteStudentUseCase.apply(request.pathVariable("id"))
+                        .flatMap(result -> ServerResponse.status(204)
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .bodyValue(result))
+                        .onErrorResume(throwable -> ServerResponse.status(HttpStatus.NOT_FOUND).build()));
+    }
 }
