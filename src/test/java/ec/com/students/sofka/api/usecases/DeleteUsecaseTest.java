7b59b6d343c1b5c2ca895d7de035854c7267c2fe
isan-san <isan.9.9f@gmail.com>
2023-03-16 23:13:50 -0500

    Finished delete useCase test

---
+++ src/test/java/ec/com/students/sofka/api/usecases/DeleteUsecaseTest.java
@@ -0,0 +1,56 @@
+package ec.com.students.sofka.api.usecases;
+
+import ec.com.students.sofka.api.domain.collection.Student;
+import ec.com.students.sofka.api.repository.IStudentRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.modelmapper.ModelMapper;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+@ExtendWith(MockitoExtension.class)
+class DeleteUsecaseTest {
+
+    @Mock
+    IStudentRepository repository;
+    DeleteUsecase deleteUsecase;
+
+    @BeforeEach
+    void init() {
+        deleteUsecase = new DeleteUsecase(repository);
+    }
+
+    @Test
+    @DisplayName("getAllStudents_Success")
+    void getAllStudents() {
+
+        Student student = new Student();
+        student.setIdNumber("Test id");
+        student.setName("Test name");
+        student.setLastName("Test last name");
+
+        Mockito.when(repository.findById("Test id")).
+                thenAnswer(InvocationOnMock -> {
+                    return Mono.just(student);
+                });
+        Mockito.when(repository.delete(student)).
+                thenAnswer(InvocationOnMock -> {
+                    return Mono.just(Void.TYPE);
+                });
+
+        Mono<String> response = deleteUsecase.apply("Test id");
+
+        StepVerifier.create(response)
+                .expectNextCount(1)
+                .verifyComplete();
+
+        Mockito.verify(repository).delete(student);
+        Mockito.verify(repository).findById("1");
+    }
+
+}
